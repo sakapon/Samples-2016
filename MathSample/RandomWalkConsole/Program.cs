5310388b0c3813ce214583980b77187fdd6a4b70
Keiho Sakapon <saka_pon@live.jp>
2016-02-04 16:10:56 +0900

    Implement random walk for 2D

--- MathSample/RandomWalkConsole/Program.cs
+++ MathSample/RandomWalkConsole/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RandomWalkConsole
 {
@@ -10,6 +9,28 @@ namespace RandomWalkConsole
     {
         static void Main(string[] args)
         {
+            var unfinished = Enumerable.Range(0, 1000)
+                .Select(_ => Walk2())
+                .Aggregate(0, (u, t) => u + (t.HasValue ? 0 : 1));
+            Console.WriteLine(unfinished);
+        }
+
+        static readonly Random random = new Random();
+        static readonly Int32Vector3[] Directions2 = new[] { Int32Vector3.XBasis, Int32Vector3.YBasis, -Int32Vector3.XBasis, -Int32Vector3.YBasis };
+
+        static int? Walk2()
+        {
+            var current = Int32Vector3.Zero;
+
+            for (var i = 1; i <= 1000000; i++)
+            {
+                current += Directions2[random.Next(0, Directions2.Length)];
+
+                if (current == Int32Vector3.Zero) return i;
+            }
+
+            Console.WriteLine(current);
+            return null;
         }
     }
 }
