3dbfbbe2ea5ee9881c48ebddbe169355a13cd40e
Keiho Sakapon <saka_pon@live.jp>
2016-02-05 12:25:03 +0900

    Add method for parallel

--- MathSample/RandomWalkConsole/Program.cs
+++ MathSample/RandomWalkConsole/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Threading.Tasks;
 using static System.Console;
 using static System.Math;
 
@@ -13,19 +14,52 @@ namespace RandomWalkConsole
         const int MaxSteps = 100 * 1000 * 1000;
         const int MaxDistance = 10 * 1000;
 
+        static readonly Int32Vector3[] Directions2 = new[] { Int32Vector3.XBasis, Int32Vector3.YBasis, -Int32Vector3.XBasis, -Int32Vector3.YBasis };
+        static readonly Int32Vector3[] Directions3 = new[] { Int32Vector3.XBasis, Int32Vector3.YBasis, Int32Vector3.ZBasis, -Int32Vector3.XBasis, -Int32Vector3.YBasis, -Int32Vector3.ZBasis };
+
+        static readonly Random RandomMaster = new Random();
+
+        static Random NextRandomizer()
+        {
+            lock (RandomMaster)
+            {
+                return new Random(RandomMaster.Next());
+            }
+        }
+
         static void Main(string[] args)
         {
+            //TryWalks();
+            TryWalksInParallel();
+        }
+
+        static void TryWalks()
+        {
             var returned = Enumerable.Range(0, Trials)
-                .Select(_ => Walk(Directions2))
+                .Select(_ => Walk(Directions2, RandomMaster))
                 .Aggregate(0, (u, t) => u + (t.HasValue ? 1 : 0));
             WriteLine($"Returned Rate: {(double)returned / Trials}");
         }
 
-        static readonly Random random = new Random();
-        static readonly Int32Vector3[] Directions2 = new[] { Int32Vector3.XBasis, Int32Vector3.YBasis, -Int32Vector3.XBasis, -Int32Vector3.YBasis };
-        static readonly Int32Vector3[] Directions3 = new[] { Int32Vector3.XBasis, Int32Vector3.YBasis, Int32Vector3.ZBasis, -Int32Vector3.XBasis, -Int32Vector3.YBasis, -Int32Vector3.ZBasis };
+        static void TryWalksInParallel()
+        {
+            var returned = 0;
+            var returnedLock = new object();
 
-        static int? Walk(Int32Vector3[] directions)
+            Parallel.For(0, Trials, _ =>
+            {
+                var steps = Walk(Directions2, NextRandomizer());
+                if (!steps.HasValue) return;
+                lock (returnedLock)
+                {
+                    returned++;
+                }
+            });
+
+            WriteLine($"Returned Rate: {(double)returned / Trials}");
+        }
+
+        static int? Walk(Int32Vector3[] directions, Random random)
         {
             var current = Int32Vector3.Zero;
 
