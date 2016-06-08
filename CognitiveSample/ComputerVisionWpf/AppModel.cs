39a999b5e8b00c7f366582abfe7fceb91038fbcc
Keiho Sakapon <saka_pon@live.jp>
2016-06-08 17:26:15 +0900

    Add classes

---
+++ CognitiveSample/ComputerVisionWpf/AppModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using Microsoft.ProjectOxford.Vision;
+using Microsoft.ProjectOxford.Vision.Contract;
+using Reactive.Bindings;
+
+namespace ComputerVisionWpf
+{
+    public class AppModel
+    {
+        static readonly string SubscriptionKey = ConfigurationManager.AppSettings["SubscriptionKey"];
+
+        VisionServiceClient Client { get; } = new VisionServiceClient(SubscriptionKey);
+
+        public ReactiveProperty<string> ImageUrl { get; } = new ReactiveProperty<string>("");
+        public ReactiveProperty<AnalysisResult> Result { get; } = new ReactiveProperty<AnalysisResult>();
+
+        public async void Analyze()
+        {
+            if (string.IsNullOrWhiteSpace(ImageUrl.Value)) return;
+
+            Result.Value = await Client.AnalyzeImageAsync(ImageUrl.Value, (VisualFeature[])Enum.GetValues(typeof(VisualFeature)));
+        }
+    }
+}
