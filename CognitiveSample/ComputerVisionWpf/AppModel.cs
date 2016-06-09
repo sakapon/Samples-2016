af27c0f9f44e93d4881a1be4d4d2195e76f46f8c
Keiho Sakapon <saka_pon@live.jp>
2016-06-09 16:38:54 +0900

    Add OCR

--- CognitiveSample/ComputerVisionWpf/AppModel.cs
+++ CognitiveSample/ComputerVisionWpf/AppModel.cs
@@ -17,17 +17,28 @@ namespace ComputerVisionWpf
         const string DefaultImageUrl = "https://model.foto.ne.jp/free/img/images_big/m010078.jpg";
 
         public ReactiveProperty<string> ImageUrl { get; } = new ReactiveProperty<string>(DefaultImageUrl);
-        public ReactiveProperty<AnalysisResult> Result { get; } = new ReactiveProperty<AnalysisResult>();
+        public ReactiveProperty<AnalysisResult> AnalysisResult { get; } = new ReactiveProperty<AnalysisResult>();
+        public ReactiveProperty<OcrResults> OcrResults { get; } = new ReactiveProperty<OcrResults>();
 
         public async void Analyze()
         {
             if (string.IsNullOrWhiteSpace(ImageUrl.Value)) return;
 
-            Result.Value = null;
+            AnalysisResult.Value = null;
+            OcrResults.Value = null;
 
             try
             {
-                Result.Value = await Client.AnalyzeImageAsync(ImageUrl.Value, (VisualFeature[])Enum.GetValues(typeof(VisualFeature)));
+                AnalysisResult.Value = await Client.AnalyzeImageAsync(ImageUrl.Value, (VisualFeature[])Enum.GetValues(typeof(VisualFeature)));
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+
+            try
+            {
+                OcrResults.Value = await Client.RecognizeTextAsync(ImageUrl.Value);
             }
             catch (Exception ex)
             {
