d7a644f5b100551660c78e577983247f8f38ba40
Keiho Sakapon <saka_pon@live.jp>
2016-06-09 20:09:21 +0900

    Update layout

--- CognitiveSample/ComputerVisionWpf/AppModel.cs
+++ CognitiveSample/ComputerVisionWpf/AppModel.cs
@@ -20,20 +20,19 @@ namespace ComputerVisionWpf
         public ReactiveProperty<string> ImageUrl { get; } = new ReactiveProperty<string>(DefaultImageUrl);
         public ReactiveProperty<AnalysisResult> AnalysisResult { get; } = new ReactiveProperty<AnalysisResult>();
         public ReactiveProperty<OcrResults> OcrResults { get; } = new ReactiveProperty<OcrResults>();
-        public ReadOnlyReactiveProperty<string[]> OcrLines { get; }
+        public ReadOnlyReactiveProperty<string> OcrLines { get; }
 
         public AppModel()
         {
             OcrLines = OcrResults
-                .Select(ocr => ocr == null ? new string[0] : ToLines(ocr))
+                .Select(ocr => ocr == null ? "" : string.Join("\r\n", ToLines(ocr)))
                 .ToReadOnlyReactiveProperty();
         }
 
-        static string[] ToLines(OcrResults ocr) =>
+        static IEnumerable<string> ToLines(OcrResults ocr) =>
             ocr.Regions
                 .SelectMany(r => r.Lines)
-                .Select(ToText)
-                .ToArray();
+                .Select(ToText);
 
         static string ToText(Line line) =>
             string.Join(" ", line.Words.Select(w => w.Text));
