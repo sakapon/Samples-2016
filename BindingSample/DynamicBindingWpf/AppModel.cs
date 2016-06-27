dc315263c50f758a419498f3ccca451b6af520db
Keiho Sakapon <saka_pon@live.jp>
2016-06-27 17:31:15 +0900

    Add classes

---
+++ BindingSample/DynamicBindingWpf/AppModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DynamicBindingWpf
+{
+    public class AppModel
+    {
+        public dynamic TextModel { get; } = new TextModel().ToDynamicNotifiable();
+    }
+
+    public class TextModel
+    {
+        string input;
+        public string Input
+        {
+            get { return input; }
+            set
+            {
+                input = value;
+                Output = input?.ToUpper();
+            }
+        }
+
+        public string Output { get; private set; }
+    }
+}
---
+++ BindingSample/DynamicBindingWpf/DynamicNotifiable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Dynamic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DynamicBindingWpf
+{
+    public static class DynamicNotifiable
+    {
+        public static DynamicNotifiable<T> ToDynamicNotifiable<T>(this T target) => new DynamicNotifiable<T>(target);
+    }
+
+    public class DynamicNotifiable<T> : DynamicObject, INotifyPropertyChanged
+    {
+        T Target { get; }
+
+        public DynamicNotifiable(T target)
+        {
+            Target = target;
+        }
+
+        public T GetTargetObject() => Target;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propertyName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
