0ca5f2f68301a9664b634e1c3159b8a23e973c9d
Keiho Sakapon <saka_pon@live.jp>
2016-06-27 19:54:36 +0900

    Add basic data binding

---
+++ BindingSample/BindingConsole/Person.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace BindingConsole
+{
+    public class Person0
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
--- BindingSample/BindingConsole/Program.cs
+++ BindingSample/BindingConsole/Program.cs
@@ -1,15 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace BindingConsole
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
+            // Binding Source (Any object).
+            var person = new Person0 { Id = 0, Name = "Taro" };
+
+            // Binding Target must be FrameworkElement.
+            var textBlock = new TextBlock { Text = "Default" };
+            Console.WriteLine(textBlock.Text);
+
+            // Binds target to source.
+            var binding = new Binding(nameof(person.Name)) { Source = person };
+            textBlock.SetBinding(TextBlock.TextProperty, binding);
+            Console.WriteLine(textBlock.Text);
+
+            // Changes value.
+            person.Name = "Jiro";
+            Console.WriteLine(textBlock.Text);
         }
     }
 }
