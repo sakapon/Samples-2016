5d1eaf6888e873a394018c45ed46607e1edc8968
Keiho Sakapon <saka_pon@live.jp>
2016-06-29 12:23:05 +0900

    Add AppModel class using ExpandoObject

---
+++ BindingSample/DynamicBindingWpf/AppModel2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+using System.Dynamic;
+
+namespace DynamicBindingWpf
+{
+    public class AppModel2
+    {
+        public dynamic TextModel { get; } = new ExpandoObject();
+
+        public AppModel2()
+        {
+            ((INotifyPropertyChanged)TextModel).PropertyChanged += (o, e) =>
+            {
+                if (e.PropertyName == "Input")
+                    TextModel.Output = TextModel.Input?.ToUpper();
+            };
+        }
+    }
+}
