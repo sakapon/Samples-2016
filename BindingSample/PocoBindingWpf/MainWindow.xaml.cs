1c5e27a9b06469e9314247d63f6a7c13c99bddde
Keiho Sakapon <saka_pon@live.jp>
2016-07-17 12:23:55 +0900

    Use PropertyDescriptor

--- BindingSample/PocoBindingWpf/MainWindow.xaml.cs
+++ BindingSample/PocoBindingWpf/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,29 @@ namespace PocoBindingWpf
         public MainWindow()
         {
             InitializeComponent();
+
+            var AppModel = (AppModel)DataContext;
+
+            Loaded += (o, e) =>
+            {
+                // Notification does not work in usual way.
+                //AppModel.Input.Number = 1234;
+                SetValue(AppModel.Input, nameof(InputModel.Number), 1234);
+            };
+
+            AddValueChanged(AppModel.Input, nameof(InputModel.Number), () => Console.WriteLine(AppModel.Input.Number));
+        }
+
+        static void SetValue(object obj, string propertyName, object value)
+        {
+            var properties = TypeDescriptor.GetProperties(obj);
+            properties[propertyName].SetValue(obj, value);
+        }
+
+        static void AddValueChanged(object obj, string propertyName, Action action)
+        {
+            var properties = TypeDescriptor.GetProperties(obj);
+            properties[propertyName].AddValueChanged(obj, (o, e) => action());
         }
     }
 }
