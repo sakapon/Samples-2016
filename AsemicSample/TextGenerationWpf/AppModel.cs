c0f27e5e3977ec191314f19afe3534f989bda757
Keiho Sakapon <git@saka-pon.net>
2016-09-05 14:51:02 +0900

    Update layout

--- AsemicSample/TextGenerationWpf/AppModel.cs
+++ AsemicSample/TextGenerationWpf/AppModel.cs
@@ -28,12 +28,13 @@ namespace TextGenerationWpf
 ";
 
         public ReactiveProperty<double> FeatureWeight { get; } = new ReactiveProperty<double>(2.0);
+        public ReactiveProperty<string> SourceText { get; } = new ReactiveProperty<string>(Arinomamade_Lyrics);
         public ReactiveProperty<string> GeneratedText { get; } = new ReactiveProperty<string>("");
 
         public void GenerateText()
         {
             var generator = new OneTextGenerator { Delimiter = '\n' };
-            GeneratedText.Value = generator.Generate(Arinomamade_Lyrics.Replace("\r\n", "\n"));
+            GeneratedText.Value = generator.Generate(SourceText.Value.Replace("\r\n", "\n"));
         }
     }
 }
