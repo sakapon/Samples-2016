f641fc9da25e48def03ed79965ca62abb96e84c8
Keiho Sakapon <git@saka-pon.net>
2016-09-08 17:59:09 +0900

    Refactor

--- AsemicSample/TextGenerationWpf/AppModel.cs
+++ AsemicSample/TextGenerationWpf/AppModel.cs
@@ -7,6 +7,19 @@ namespace TextGenerationWpf
 {
     public class AppModel
     {
+        public ReactiveProperty<double> FeatureWeight { get; } = new ReactiveProperty<double>(2.0);
+        public ReactiveProperty<string> SourceText { get; } = new ReactiveProperty<string>(Arinomamade_Lyrics);
+        public ReactiveProperty<string> GeneratedText { get; } = new ReactiveProperty<string>("");
+
+        public void GenerateText()
+        {
+            var generator = new PseudoGenerator<char, string>('\n', ToString) { FeatureWeight = FeatureWeight.Value };
+            generator.Train(SourceText.Value.Replace("\r\n", "\n"));
+            GeneratedText.Value = ToString(generator.Generate());
+        }
+
+        static string ToString(IEnumerable<char> chars) => new string(chars.ToArray());
+
         const string Arinomamade_Lyrics = @"ふりはじめたゆきは
 あしあとけして
 まっしろなせかいに
@@ -24,18 +37,5 @@ namespace TextGenerationWpf
 なにもこわくない
 かぜよふけ
 すこしもさむくないわ";
-
-        public ReactiveProperty<double> FeatureWeight { get; } = new ReactiveProperty<double>(2.0);
-        public ReactiveProperty<string> SourceText { get; } = new ReactiveProperty<string>(Arinomamade_Lyrics);
-        public ReactiveProperty<string> GeneratedText { get; } = new ReactiveProperty<string>("");
-
-        public void GenerateText()
-        {
-            var generator = new PseudoGenerator<char, string>('\n', ToString) { FeatureWeight = FeatureWeight.Value };
-            generator.Train(SourceText.Value.Replace("\r\n", "\n"));
-            GeneratedText.Value = ToString(generator.Generate());
-        }
-
-        static string ToString(IEnumerable<char> chars) => new string(chars.ToArray());
     }
 }
