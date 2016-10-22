82559360742f2d379f10f7e29ab29536d256d47b
Keiho Sakapon <git@saka-pon.net>
2016-10-22 17:34:26 +0900

    Implement factorization

--- BotSample/FactorizationBotApi/Controllers/MessagesController.cs
+++ BotSample/FactorizationBotApi/Controllers/MessagesController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -19,25 +20,38 @@ namespace FactorizationBotApi
         /// </summary>
         public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
         {
-            if (activity.Type == ActivityTypes.Message)
-            {
-                ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
-                // calculate something for us to return
-                int length = (activity.Text ?? string.Empty).Length;
+            if (activity.Type != ActivityTypes.Message)
+                return Request.CreateResponse(HttpStatusCode.OK);
+            if (string.IsNullOrWhiteSpace(activity.Text))
+                return Request.CreateResponse(HttpStatusCode.OK);
 
-                // return our reply to the user
-                Activity reply = activity.CreateReply($"You sent {activity.Text} which was {length} characters");
-                await connector.Conversations.ReplyToActivityAsync(reply);
-            }
-            else
-            {
-                HandleSystemMessage(activity);
-            }
-            var response = Request.CreateResponse(HttpStatusCode.OK);
-            return response;
+            var connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+
+            var echoMessage = $"You sent \"{activity.Text}\".";
+            await Reply(connector, activity, echoMessage);
+
+            var mainMessage = Factorize(activity.Text);
+            await Reply(connector, activity, mainMessage);
+
+            return Request.CreateResponse(HttpStatusCode.OK);
         }
 
-        private Activity HandleSystemMessage(Activity message)
+        static string Factorize(string text)
+        {
+            int i;
+            if (!int.TryParse(text, out i)) return "Send an integer.";
+
+            var factorized = MathHelper.Factorize(i);
+            return $"{i} = {string.Join(" * ", factorized)}";
+        }
+
+        static async Task Reply(ConnectorClient connector, Activity activity, string message)
+        {
+            var reply = activity.CreateReply(message);
+            await connector.Conversations.ReplyToActivityAsync(reply);
+        }
+
+        Activity HandleSystemMessage(Activity message)
         {
             if (message.Type == ActivityTypes.DeleteUserData)
             {
@@ -64,6 +78,28 @@ namespace FactorizationBotApi
             }
 
             return null;
+        }
+    }
+
+    public static class MathHelper
+    {
+        public static int[] Factorize(int x)
+        {
+            if (x <= 1) return new[] { x };
+
+            var l = new List<int>();
+
+            for (var i = 2; ; i++)
+            {
+                while (x % i == 0)
+                {
+                    l.Add(i);
+                    x /= i;
+
+                    if (x == 1)
+                        return l.ToArray();
+                }
+            }
         }
     }
 }
