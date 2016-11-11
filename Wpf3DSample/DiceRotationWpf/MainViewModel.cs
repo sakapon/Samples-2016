5645a13232abdb8c2e742a82a76a7b9476fedca3
Keiho Sakapon <git@saka-pon.net>
2016-11-12 01:19:33 +0900

    Add classes

---
+++ Wpf3DSample/DiceRotationWpf/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DiceRotationWpf
+{
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+        }
+    }
+}
