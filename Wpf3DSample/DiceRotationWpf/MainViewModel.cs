3828ee94c1d21302c09af5834033b30b844c7bf3
Keiho Sakapon <git@saka-pon.net>
2016-11-12 11:38:38 +0900

    Refactor

--- Wpf3DSample/DiceRotationWpf/MainViewModel.cs
+++ Wpf3DSample/DiceRotationWpf/MainViewModel.cs
@@ -7,21 +7,21 @@ namespace DiceRotationWpf
 {
     public class MainViewModel
     {
-        public Transform3D CubeTransform { get; private set; }
+        public Transform3D CubeTransform { get; }
 
         MatrixTransform3D matrixTransform = new MatrixTransform3D();
 
         public MainViewModel()
         {
-            InitializeCube();
+            CubeTransform = InitializeCubeTransform();
         }
 
-        void InitializeCube()
+        Transform3D InitializeCubeTransform()
         {
             var transform = new Transform3DGroup();
             transform.Children.Add(matrixTransform);
             transform.Children.Add(new RotateTransform3D(new AxisAngleRotation3D(new Vector3D(0.6, 0.3, 0.7), -70)));
-            CubeTransform = transform;
+            return transform;
         }
     }
 }
