c9b9720fb11f96194ad67b37b670d8cbc3959962
Keiho Sakapon <git@saka-pon.net>
2016-11-14 18:41:17 +0900

    Improve code

--- Wpf3DSample/DiceRotationWpf/EventsExtension.cs
+++ Wpf3DSample/DiceRotationWpf/EventsExtension.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reactive.Linq;
-using System.Reactive.Subjects;
 using System.Windows;
 using System.Windows.Input;
 
@@ -13,8 +12,7 @@ namespace DiceRotationWpf
         public TElement Target { get; }
 
         Point MouseDragLastPoint;
-        Subject<Vector> _MouseDragDelta = new Subject<Vector>();
-        public IObservable<Vector> MouseDragDelta => _MouseDragDelta;
+        public IObservable<Vector> MouseDragDelta { get; }
 
         public EventsExtension(TElement target)
         {
@@ -27,15 +25,14 @@ namespace DiceRotationWpf
             var mouseLeave = Observable.FromEventPattern<MouseEventArgs>(Target, nameof(UIElement.MouseLeave)).Select(e => e.EventArgs);
             var mouseDownEnd = mouseUp.Merge(mouseLeave);
 
-            mouseDown
+            MouseDragDelta = mouseDown
                 .Select(e => e.GetPosition(Target))
                 .Do(p => MouseDragLastPoint = p)
-                .Subscribe(p0 => mouseMove
+                .SelectMany(p0 => mouseMove
                     .TakeUntil(mouseDownEnd)
                     .Select(e => new { p1 = MouseDragLastPoint, p2 = e.GetPosition(Target) })
                     .Do(_ => MouseDragLastPoint = _.p2)
-                    .Do(_ => _MouseDragDelta.OnNext(_.p2 - _.p1))
-                    .Subscribe());
+                    .Select(_ => _.p2 - _.p1));
         }
     }
 }
