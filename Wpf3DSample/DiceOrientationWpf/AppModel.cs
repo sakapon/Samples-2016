838a96d9a62233e6f05f2a29a8eeec55daa6b45a
Keiho Sakapon <git@saka-pon.net>
2016-11-22 22:45:50 +0900

    Add classes

---
+++ Wpf3DSample/DiceOrientationWpf/AppModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Threading;
+using System.Windows.Media.Media3D;
+using Reactive.Bindings;
+using Windows.Devices.Sensors;
+
+namespace DiceOrientationWpf
+{
+    public class AppModel
+    {
+        public Transform3D CubeTransform { get; }
+        MatrixTransform3D matrixTransform = new MatrixTransform3D();
+
+        public AppModel()
+        {
+            CubeTransform = InitializeCubeTransform();
+        }
+
+        Transform3D InitializeCubeTransform()
+        {
+            var transform = new Transform3DGroup();
+            transform.Children.Add(matrixTransform);
+            return transform;
+        }
+    }
+}
