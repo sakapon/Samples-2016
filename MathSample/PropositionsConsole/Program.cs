cc92fc77fdbcdba17b01131d20a11f882d90f733
Keiho Sakapon <git@saka-pon.net>
2016-12-16 15:54:56 +0900

    Add test

--- MathSample/PropositionsConsole/Program.cs
+++ MathSample/PropositionsConsole/Program.cs
@@ -19,6 +19,24 @@ namespace PropositionsConsole
             TestTautology(Imply(p, !p));
             TestContradiction(Imply(p, !p));
             TestContradiction(Equivalent(p, !p));
+            Console.WriteLine();
+
+            TestTautology(Equivalent(!(p & q), !p | !q));
+            TestTautology(Equivalent(!(p | q), !p & !q));
+            Console.WriteLine();
+
+            TestTautology(Imply(Imply(p, q), Imply(q, p)));
+            TestContradiction(Imply(Imply(p, q), Imply(q, p)));
+            TestTautology(Imply(p & Imply(p, q), q));
+            Console.WriteLine();
+
+            Console.WriteLine("三段論法：");
+            TestTautology(Imply(Imply(p, q) & Imply(q, r), Imply(p, r)));
+            Console.WriteLine("背理法：");
+            TestTautology(Imply(Imply(p, q) & Imply(p, !q), !p));
+            Console.WriteLine("対偶：");
+            TestTautology(Equivalent(Imply(p, q), Imply(!q, !p)));
+            Console.WriteLine();
         }
 
         static void TestTautology(Formula f)
