0292dd400c6f6ac75cd36ba4e275323cd8a9556d
Keiho Sakapon <git@saka-pon.net>
2016-12-23 17:19:06 +0900

    Add classes for Formula

--- MathSample/NumberGuessConsole/Program.cs
+++ MathSample/NumberGuessConsole/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Blaze.Propositions;
+using static Blaze.Propositions.Formula;
 
 namespace NumberGuessConsole
 {
@@ -10,6 +10,27 @@ namespace NumberGuessConsole
     {
         static void Main(string[] args)
         {
+        }
+    }
+
+    public static class FormulaHelper
+    {
+        // formula の中から変数を取得します。なければ作成します。
+        public static VariableFormula<TStatement> GetVariable<TStatement>(this Formula formula, TStatement statement) =>
+            formula.GetDescendants()
+                .OfType<VariableFormula<TStatement>>()
+                .Distinct()
+                .Where(v => Equals(v.Statement, statement))
+                .FirstOrDefault() ?? Variable(statement);
+
+        public static VariableFormula<TStatement> GetVariable<TStatement>(this Formula formula, VariableFormula<TStatement> variable) =>
+            formula.GetVariable(variable.Statement);
+
+        public static VariableFormula<TStatement> CreateProposition<TStatement>(TStatement statement, bool? truthValue)
+        {
+            var v = Variable(statement);
+            v.Value = truthValue;
+            return v;
         }
     }
 }
