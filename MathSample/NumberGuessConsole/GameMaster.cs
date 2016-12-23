bdc04afa233d9950d6a26bd7dbf38ee280f3cda0
Keiho Sakapon <git@saka-pon.net>
2016-12-23 17:26:08 +0900

    Add classes for game playing

---
+++ MathSample/NumberGuessConsole/GameMaster.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Blaze.Propositions;
+
+namespace NumberGuessConsole
+{
+    public class GameMaster
+    {
+        public IEnumerable<VariableFormula> Start(int numberA, int numberB)
+        {
+            var playerA = new Player(PlayerIdentity.A);
+            var playerB = new Player(PlayerIdentity.B);
+
+            playerA.Initialize(numberA);
+            playerB.Initialize(numberB);
+
+            for (var i = 1; ; i++)
+            {
+                var resultA = playerA.Guess(i);
+                var resultB = playerB.Guess(i);
+
+                yield return FormulaHelper.CreateProposition(new PlayerTurns(playerA.Id, i), resultA.HasValue);
+                yield return FormulaHelper.CreateProposition(new PlayerTurns(playerB.Id, i), resultB.HasValue);
+
+                if (resultA.HasValue) yield return FormulaHelper.CreateProposition(resultA.Value, true);
+                if (resultB.HasValue) yield return FormulaHelper.CreateProposition(resultB.Value, true);
+
+                if (resultA.HasValue || resultB.HasValue) yield break;
+            }
+        }
+    }
+
+    public class GameSimulator
+    {
+        public VariableFormula<PlayerTurns> Start(PlayerNumber[] numbers, PlayerIdentity targetId)
+        {
+            var targetNumber = numbers.First(n => n.PlayerId == targetId).Number;
+            var opponentNumber = numbers.First(n => n.PlayerId == targetId.Opponent).Number;
+
+            var targetPlayer = new SimulationPlayer(targetId);
+            var opponentPlayer = new SimulationPlayer(targetId.Opponent);
+
+            targetPlayer.Initialize(numbers, this);
+            opponentPlayer.Initialize(numbers, this);
+
+            for (var i = 1; ; i++)
+            {
+                var targetResult = targetPlayer.Guess(i);
+                var opponentResult = opponentPlayer.Guess(i);
+
+                if (targetResult.HasValue) return FormulaHelper.CreateProposition(new PlayerTurns(targetId, i), true);
+                if (opponentResult.HasValue) return FormulaHelper.CreateProposition(new PlayerTurns(targetId.Opponent, i), true);
+
+                if (targetNumber > opponentNumber && i == targetNumber - 2)
+                    return FormulaHelper.CreateProposition(new PlayerTurns(targetId, i), false);
+            }
+        }
+
+        [Obsolete]
+        public VariableFormula<PlayerTurns> Start_Stub(PlayerNumber[] numbers, PlayerIdentity targetId)
+        {
+            var targetNumber = numbers.First(n => n.PlayerId == targetId).Number;
+            var opponentNumber = numbers.First(n => n.PlayerId == targetId.Opponent).Number;
+
+            return targetNumber < opponentNumber ?
+                FormulaHelper.CreateProposition(new PlayerTurns(targetId, targetNumber), true) :
+                FormulaHelper.CreateProposition(new PlayerTurns(targetId, targetNumber - 2), false);
+        }
+    }
+}
---
+++ MathSample/NumberGuessConsole/Player.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Blaze.Propositions;
+using static Blaze.Propositions.Formula;
+
+namespace NumberGuessConsole
+{
+    public abstract class PlayerBase
+    {
+        public PlayerIdentity Id { get; }
+
+        protected PlayerBase(PlayerIdentity id)
+        {
+            Id = id;
+        }
+
+        public override string ToString() => $"{Id}";
+
+        protected VariableFormula<PlayerNumber>[] Candidates { get; set; }
+        protected Formula Knowledge { get; set; }
+
+        public PlayerNumber? Guess(int turns)
+        {
+            if (turns > 1)
+            {
+                var previous = Knowledge.GetVariable(new PlayerTurns(Id.Opponent, turns - 1));
+                // kb &= !previous; とすると処理が増えるため、最適化します。
+                previous.Value = false;
+            }
+
+            foreach (var v in Candidates)
+            {
+                Knowledge.Determine(v);
+                if (v.Value == true)
+                    return v.Statement;
+            }
+
+            return null;
+        }
+    }
+
+    public class Player : PlayerBase
+    {
+        public Player(PlayerIdentity id) : base(id) { }
+
+        public void Initialize(int selfNumber)
+        {
+            var self = new PlayerNumber(Id, selfNumber);
+            Candidates = GameRules.GetCandidates(self).Select(Variable).ToArray();
+            Knowledge = Xor(Candidates);
+
+            if (Candidates.Length <= 1) return;
+
+            var simulator = new GameSimulator();
+            foreach (var v in Candidates)
+            {
+                var simulationResult = simulator.Start(new[] { self, v.Statement }, Id.Opponent);
+                var r = Knowledge.GetVariable(simulationResult);
+                Knowledge &= Imply(v, simulationResult.Value == true ? r : !r);
+            }
+        }
+    }
+
+    public class SimulationPlayer : PlayerBase
+    {
+        public SimulationPlayer(PlayerIdentity id) : base(id) { }
+
+        public void Initialize(PlayerNumber[] numbers, GameSimulator simulator)
+        {
+            var self = numbers.First(n => n.PlayerId == Id);
+            var opponent = numbers.First(n => n.PlayerId == Id.Opponent);
+            Candidates = GameRules.GetCandidates(self).Select(Variable).ToArray();
+            Knowledge = Xor(Candidates);
+
+            if (Candidates.Length <= 1) return;
+
+            // 特定のプレーヤーの思考の中で無限ループになる場合、「わからない」と結論付けます (知識を追加しない)。
+            foreach (var v in Candidates.Where(c => c.Statement.Number < opponent.Number))
+            {
+                var simulationResult = simulator.Start(new[] { self, v.Statement }, Id.Opponent);
+                var r = Knowledge.GetVariable(simulationResult);
+                Knowledge &= Imply(v, simulationResult.Value == true ? r : !r);
+            }
+        }
+    }
+}
