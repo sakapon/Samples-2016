b54490d44d8424edc6892a6c485d959d475c97e6
Keiho Sakapon <git@saka-pon.net>
2016-12-23 17:30:53 +0900

    Play game

--- MathSample/NumberGuessConsole/Program.cs
+++ MathSample/NumberGuessConsole/Program.cs
@@ -1,15 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using Blaze.Propositions;
+using static System.Console;
 using static Blaze.Propositions.Formula;
 
 namespace NumberGuessConsole
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
+            var master = new GameMaster();
+
+            master.Start(1, 2).WriteLines();
+            WriteLine();
+            master.Start(2, 3).WriteLines();
+            WriteLine();
+            master.Start(3, 4).WriteLines();
+            WriteLine();
+
+            master.Start(2, 1).WriteLines();
+            WriteLine();
+            master.Start(3, 2).WriteLines();
+            WriteLine();
+            master.Start(4, 3).WriteLines();
+            WriteLine();
+
+            master.Start(100, 101).WriteLines();
+            WriteLine();
+        }
+
+        [DebuggerHidden]
+        static void WriteLines(this IEnumerable<VariableFormula> source)
+        {
+            foreach (var v in source)
+                WriteLine($"{v}: {v.Value}");
         }
     }
 
