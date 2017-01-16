ef9be26e354aa0c3295171aafebbeb5d21b65803
Keiho Sakapon <git@saka-pon.net>
2017-01-16 14:22:26 +0900

    Show dice

--- LeapSample/HandRotationLeap/MainWindow.xaml.cs
+++ LeapSample/HandRotationLeap/MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
@@ -20,9 +21,34 @@ namespace HandRotationLeap
     /// </summary>
     public partial class MainWindow : Window
     {
+        public Model3D CubeModel
+        {
+            get { return (Model3D)GetValue(CubeModelProperty); }
+            set { SetValue(CubeModelProperty, value); }
+        }
+
+        public static readonly DependencyProperty CubeModelProperty =
+            DependencyProperty.Register(nameof(CubeModel), typeof(Model3D), typeof(MainWindow), new PropertyMetadata(null));
+
         public MainWindow()
         {
             InitializeComponent();
+
+            InitializeCube();
+        }
+
+        void InitializeCube()
+        {
+            var faces = new Dictionary<string, string>
+            {
+                { "-1,1,1 -1,-1,1 1,-1,1 1,1,1", "Face1" },
+                { "-1,1,1 -1,1,-1 -1,-1,-1 -1,-1,1", "Face2" },
+                { "-1,-1,1 -1,-1,-1 1,-1,-1 1,-1,1", "Face3" },
+                { "1,1,1 1,1,-1 -1,1,-1 -1,1,1", "Face4" },
+                { "1,-1,1 1,-1,-1 1,1,-1 1,1,1", "Face5" },
+                { "-1,-1,-1 -1,1,-1 1,1,-1 1,-1,-1", "Face6" },
+            };
+            CubeModel = CubeUtility.CreateCubeModel(faces);
         }
     }
 }
