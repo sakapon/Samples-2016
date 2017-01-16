2b42b48ad53c012c14928895dcae7cd32b7f6d4a
Keiho Sakapon <git@saka-pon.net>
2017-01-16 14:21:13 +0900

    Add classes

---
+++ LeapSample/HandRotationLeap/AppModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Threading;
+using System.Windows.Media.Media3D;
+using Monsoon.Reactive.Leap;
+using Reactive.Bindings;
+
+namespace HandRotationLeap
+{
+    public class AppModel
+    {
+        public Transform3D CubeTransform { get; }
+        MatrixTransform3D matrixTransform = new MatrixTransform3D();
+
+        public AppModel()
+        {
+            CubeTransform = InitializeCubeTransform();
+        }
+
+        Transform3D InitializeCubeTransform()
+        {
+            var transform = new Transform3DGroup();
+            transform.Children.Add(new RotateTransform3D(new AxisAngleRotation3D(new Vector3D(-0.8, 0.3, 0.5), 60)));
+            transform.Children.Add(matrixTransform);
+            return transform;
+        }
+    }
+}
