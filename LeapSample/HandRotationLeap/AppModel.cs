0a8fdfa4dec8c700ff9478f2464eeacd5dad58bc
Keiho Sakapon <git@saka-pon.net>
2017-01-16 15:03:33 +0900

    Show hand rotation

--- LeapSample/HandRotationLeap/AppModel.cs
+++ LeapSample/HandRotationLeap/AppModel.cs
@@ -14,9 +14,46 @@ namespace HandRotationLeap
         public Transform3D CubeTransform { get; }
         MatrixTransform3D matrixTransform = new MatrixTransform3D();
 
+        public LeapManager LeapManager { get; } = new LeapManager();
+
+        public ReadOnlyReactiveProperty<Leap.Hand> FrontHand { get; }
+        public ReadOnlyReactiveProperty<double> InnerProduct { get; }
+
+        public ReadOnlyReactiveProperty<EulerAngles> EulerAngles_org { get; }
+        public ReadOnlyReactiveProperty<EulerAngles> EulerAngles { get; }
+
+        public ReadOnlyReactiveProperty<Matrix3D> Rotation_org { get; }
+        public ReadOnlyReactiveProperty<Matrix3D> Rotation { get; }
+
         public AppModel()
         {
             CubeTransform = InitializeCubeTransform();
+
+            FrontHand = LeapManager.FrameArrived
+                .Select(f => f.Hands.Frontmost)
+                .Select(h => h?.IsValid == true ? h : null)
+                .ToReadOnlyReactiveProperty();
+            InnerProduct = FrontHand
+                .Select(h => h != null ? h.Direction.Dot(h.PalmNormal) : double.NaN)
+                .ToReadOnlyReactiveProperty();
+
+            EulerAngles_org = FrontHand
+                .Select(h => h != null ? h.GetEulerAngles_org() : new EulerAngles())
+                .ToReadOnlyReactiveProperty();
+            EulerAngles = FrontHand
+                .Select(h => h != null ? h.GetEulerAngles() : new EulerAngles())
+                .ToReadOnlyReactiveProperty();
+
+            Rotation_org = EulerAngles_org
+                .Select(Rotation3DHelper.ToMatrix3D)
+                .ToReadOnlyReactiveProperty();
+            Rotation = EulerAngles
+                .Select(Rotation3DHelper.ToMatrix3D)
+                .ToReadOnlyReactiveProperty();
+
+            Rotation
+                .ObserveOn(SynchronizationContext.Current)
+                .Subscribe(m => matrixTransform.Matrix = m);
         }
 
         Transform3D InitializeCubeTransform()
@@ -26,5 +63,20 @@ namespace HandRotationLeap
             transform.Children.Add(matrixTransform);
             return transform;
         }
+    }
+
+    public static class Leap3DHelper
+    {
+        public static Vector3D ToVector3D(this Leap.Vector v) => new Vector3D(v.x, v.y, v.z);
+
+        public static EulerAngles GetEulerAngles_org(this Leap.Hand h) => new EulerAngles
+        {
+            Yaw = h.Direction.Yaw,
+            Pitch = h.Direction.Pitch,
+            Roll = h.PalmNormal.Roll,
+        };
+
+        public static EulerAngles GetEulerAngles(this Leap.Hand h) =>
+            Rotation3DHelper.ToEulerAngles(-h.Direction.ToVector3D(), -h.PalmNormal.ToVector3D());
     }
 }
